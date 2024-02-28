4811eb46ff3c84a8969d2ecb5a53ca15a98726d9
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2024-02-28 13:34:02 +0200

    feat: add solution opened scene warning

--- addons/gdquest_practice_framework/gdquest_practice_framework.gd
+++ addons/gdquest_practice_framework/gdquest_practice_framework.gd
@@ -1,16 +1,28 @@
 @tool
 extends EditorPlugin
 
+const Metadata := preload("metadata.gd")
 const Utils := preload("utils.gd")
+const Paths := preload("paths.gd")
+const UISolutionWarning := preload("ui/ui_solution_warning.gd")
+
+const PracticeMetadata := Metadata.PracticeMetadata
 
 const ROOT_PATH := "res://"
 const TEMPLATES_DIR := "script_templates/Test"
 
+var metadata: Array[PracticeMetadata] = Metadata.load()
+
 var editor_run_bar: MarginContainer = null
+
 var ui_practice_dock := preload("ui/ui_practice_dock.tscn").instantiate()
+var ui_solution_warning: UISolutionWarning = preload("ui/ui_solution_warning.tscn").instantiate()
 
 
 func _enter_tree() -> void:
+	scene_changed.connect(_on_scene_changed)
+	EditorInterface.get_editor_main_screen().add_child(ui_solution_warning)
+
 	editor_run_bar = EditorInterface.get_base_control().find_child("*EditorRunBar*", true, false)
 	editor_run_bar.stop_pressed.connect(ui_practice_dock.update)
 	scene_changed.connect(ui_practice_dock.select_practice)
@@ -27,6 +39,26 @@ func _exit_tree() -> void:
 	scene_changed.disconnect(ui_practice_dock.select_practice)
 	remove_control_from_docks(ui_practice_dock)
 	ui_practice_dock.queue_free()
+	ui_solution_warning.queue_free()
+
+
+func _on_scene_changed(scene_root: Node) -> void:
+	var is_solution := scene_root != null
+	is_solution = is_solution and scene_root.scene_file_path.begins_with(Paths.SOLUTIONS_PATH)
+	ui_solution_warning.visible = is_solution
+	if not is_solution:
+		return
+
+	var solution_file_path := scene_root.scene_file_path
+	var solution_dir_path := solution_file_path.get_base_dir()
+	var predicate := func(m: PracticeMetadata) -> bool: return m.main_scene.begins_with(
+		solution_dir_path
+	)
+	for pm: PracticeMetadata in metadata.filter(predicate):
+		var practice_title := "L%d.P%d %s" % [pm.lesson_number, pm.practice_number, pm.title]
+		ui_solution_warning.set_text(
+			solution_file_path, Paths.to_practice(pm.main_scene), practice_title
+		)
 
 
 ## Copies practice template scripts to the project root directory.
